8a7c9ee7dda092ffd98c2dd7ac50c597adf41201
poloport <poloport@users.noreply.github.com>
2018-09-24 23:28:34 +0200

    clarified the queues.sh

--- queues.sh
+++ queues.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
+create queue Queue_A
+create topic Topic_A
 
-
-create queue queue
-create queue queue1
-
-create topic topic
-
-create bridge source=topic:topic target=queue:queue1
+create bridge source=topic:Topic_A target=queue:Queue_A
 
 setprop factory TopicConnectionFactory url=tcp://localhost:7222
 setprop factory QueueConnectionFactory url=tcp://localhost:7222
